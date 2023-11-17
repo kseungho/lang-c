07d7dca00a9b724618eb94d8cfefcc7aa38e4a2a
kseungho <147792526+kseungho@users.noreply.github.com>
2023-11-17 14:30:33 +0900

    Add files via upload

---
+++ 2020531067_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+    printf("Welcome to C programming! \n");
+    int x = 10 ;
+    int y = 2 * x ;
+    int z = x + y ;
+    printf("x = %d \n" , x);
+    printf("y = %d \n" , y);
+    printf("z = %d \n" , z);
+    int price ; 
+    price = 500 ;
+    float A;
+    A = price * 1.1 ;
+    printf("%f \n" , A);
+}
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) 
+{
+    printf("hello\n");
+    printf("i'm from \n");
+    printf("Mars\n");
+    printf("%d\n" , 5877);
+    printf("%d\n" , 1234+572);
+    printf("%f\n" , 512.0+567.5);
+    int price;
+    price = 1321+84;
+    printf("%d\n" , price);
+    return 0;
+}
